f750346243ac6d14a05aaffd05348087d67756ac
Klaus Fiedler <klaus@tollwerk.de>
2026-09-25 13:38:09 +0200

    Fix TypoScript constants not working anymore. Migrate to site settings.

--- Classes/ViewHelpers/IconViewHelper.php
+++ Classes/ViewHelpers/IconViewHelper.php
@@ -7,7 +7,7 @@
  * @package    Tollwerk\TwIcons
  * @subpackage Tollwerk\TwIcons\ViewHelpers
  * @author     tollwerk GmbH <info@tollwerk.de>
- * @copyright  2023 tollwerk GmbH <info@tollwerk.de>
+ * @copyright  2026 tollwerk GmbH <info@tollwerk.de>
  * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License 3
  * @link       https://tollwerk.de
  */
@@ -17,28 +17,28 @@ namespace Tollwerk\TwIcons\ViewHelpers;
 use DOMAttr;
 use DOMDocument;
 use OutOfBoundsException;
+use Psr\Http\Message\ServerRequestInterface;
 use Tollwerk\TwIcons\Utility\SvgIconManager;
 use Tollwerk\TwIcons\ViewHelpers\Icon\IconViewHelperTrait;
-use TYPO3\CMS\Extbase\Configuration\Exception\InvalidConfigurationTypeException;
+use TYPO3\CMS\Core\Resource\Exception\InvalidConfigurationException;
+use TYPO3\CMS\Core\Site\Entity\Site;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManager;
 use Exception;
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper;
 
 /**
  * Render a single SVG icon
- *
- * @category   Tollwerk\TwIcons\ViewHelpers
- * @package    Tollwerk\TwIcons
- * @subpackage Tollwerk\TwIcons\ViewHelpers
- * @author     Tollwerk GmbH <info@tollwerk.de>
- * @license    MIT https://opensource.org/licenses/MIT
- * @link       https://tollwerk.de
  */
 class IconViewHelper extends AbstractTagBasedViewHelper
 {
     /**
-     * Use shared icon viewhelper methods
+     * Icon root paths
+     *
+     * @var string[]|null
      */
-    use IconViewHelperTrait;
+    protected static $iconRootPaths = null;
 
     /**
      * Icon types
@@ -53,6 +53,64 @@ class IconViewHelper extends AbstractTagBasedViewHelper
      * @var string
      */
     protected $tagName = 'svg';
+
+    /**
+     * Get Request object
+     *
+     * @return ServerRequestInterface|null
+     */
+    private function getRequest(): ServerRequestInterface|null
+    {
+        if ($this->renderingContext->hasAttribute(ServerRequestInterface::class)) {
+            return $this->renderingContext->getAttribute(ServerRequestInterface::class);
+        }
+        return null;
+    }
+
+    /**
+     * Find an icon and return the absolute icon path
+     *
+     * @param string $icon Icon name
+     *
+     * @return string Icon file path
+     * @throws Exception
+     * @throws InvalidConfigurationException
+     */
+    protected function getIconFile(string $icon): string
+    {
+        // Search for the icon in the given icon root path order
+        foreach ($this->getIconRootPaths() as $iconRootPath) {
+            $iconFile = GeneralUtility::getFileAbsFileName($iconRootPath . $icon);
+            if (is_file($iconFile)) {
+                return $iconFile;
+            }
+        }
+
+        throw new OutOfBoundsException($icon, 1549185715);
+    }
+
+    /**
+     * Return the list of icon root paths
+     *
+     * @return string[] Icon root paths
+     * @throws InvalidConfigurationException
+     * @throws Exception
+     */
+    protected function getIconRootPaths(): array
+    {
+        if (self::$iconRootPaths === null) {
+            self::$iconRootPaths  = [];
+            $request = $this->getRequest();
+            $site = $request->getAttribute('site');
+            self::$iconRootPaths  = array_map(
+                function ($rootPath) {
+                    return rtrim($rootPath, '/') . '/';
+                },
+                GeneralUtility::trimExplode(',', $site->getSettings()->get('twIcons.iconRootPath'), true)
+            );
+        }
+        return self::$iconRootPaths;
+    }
 
     /**
      * Initialize arguments
@@ -98,7 +156,7 @@ class IconViewHelper extends AbstractTagBasedViewHelper
             return $this->tag->render();
         } catch (OutOfBoundsException $e) {
             if ($this->arguments['debug']) {
-                return '<!-- Unknown SVG icon "' . $e->getMessage() . '" -->';
+                return '<!-- Unknown SVG icon "' . $e->getMessage() . '". Please check site setting "twIcons.iconRootPath". -->';
             }
 
             return '';
